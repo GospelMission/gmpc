54e37cb14032cf25b84978b43e50b48b9dff8a56
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 17:00:33 +0800

    try test

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/gmpc'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
