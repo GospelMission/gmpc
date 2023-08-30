f837430146cdced2d0614ad7ca7c32fb891d9d8f
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 18:12:53 +0800

    try fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-import NavBar from './components/NavBar'
+import { useState } from 'react'
+import { Link, Route, Routes} from 'react-router-dom'
+import { FaBars, FaTimes } from "react-icons/fa"
+import Home from './components/pages/Home'
+import DiscipleshipGuide from "./components/pages/DiscipleshipGuide"
 
-function App() {
-  return <NavBar />
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleClick() {
+    if(!isOpen) {
+      setIsOpen(prevValue => !prevValue)
+    } else {
+      setIsOpen(prevValue => !prevValue)
+    }
+  }
+
+  return (
+  <>
+    <div className={isOpen ? 'hide' : ''}>
+      <header>
+          <span className='website-name'>Logo Here</span>
+          <button className='nav-btn' onClick={handleClick}><FaBars className='nav-icon'/></button>
+          <nav className={isOpen ? 'nav-links' : 'hide'}>
+              <Link to="/" onClick={() => setIsMenuOpen(false)}>Start Here</Link>
+              <Link to="/guide" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
+          </nav>
+      </header>
+    </div>
+
+    <div className={isOpen ? 'mobile-nav-links' : 'hide'}>
+        <nav>
+            <button className='nav-btn' onClick={handleClick}><FaTimes className='nav-icon'/></button>
+            <Link to="/" onClick={() => setIsMenuOpen(false)}>Start Here</Link>
+            <Link to="/guide" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
+        </nav>
+    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/guide" element={<DiscipleshipGuide />} />
+      </Routes>
+  </>
+  )
 }
 
-export default App
+export default NavBar
